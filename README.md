250c8892e04d8352bfd17812bf74b36bce9aa9d8
Vincent Bailly <vibailly@microsoft.com>
2021-02-02 10:56:10 +0100

    define test cases

--- README.md
+++ README.md
@@ -28,18 +28,3 @@ installLocalStore({
 The inputs are:
 - The location of a store. This is where the packages will be installed on disk
 - The dependency graph of external packages. Contains a list of node and a list of links between the nodes.
-
-
-Test cases (TODO: implement these tests)
-
-- a package with a bin field which is a string
-     - When the package name is simple
-     - When the package is in a namespace
-- a package with a bin field which is an object
-- a package with no bin field
-- all packages are installed
-     - packages with a simple name
-     - packages with a namespaced name
-- node_modules is properly created
-- the location given does not exist
-- the location given already exists
--- index.test.ts
+++ index.test.ts
@@ -3,3 +3,27 @@ import { installLocalStore } from ".";
 it("finds the function", () => {
     expect(typeof installLocalStore).toBe("function");
 })
+
+/**
+ * Tests to add
+ * - Input validation:
+ *   - location
+ *     - location path does not already exist on disk
+ *     - location's parent does exist on disk
+ *   - dependenyGraph
+ *     - contains nodes which is an array
+ *     - contains links which is an array
+ *     - each node have a key and location
+ *     - each node key is unique
+ *     - each location on disk exists and are folders
+ *     - each link contain a source and a target
+ *     - all sources and targets refer to a key that exists
+ * - Scenarios:
+ *   - empty nodes and empty links
+ *   - a few nodes but no links
+ *   - a few nodes and a few links
+ *   - a package name has a namespace
+ *   - a package with a namespace name has a bin field which is a string
+ *   - a package with a bin field which is an object
+ *   - circular dependencies are supported
+ */
