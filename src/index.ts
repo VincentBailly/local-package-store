ccfad127fecb9f0483ab7c62deefc2345b37a689
Vincent Bailly <vibailly@microsoft.com>
2021-02-18 10:34:39 +0100

    extract file copy facility in its own module

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import * as fs from "fs";
 import * as path from "path";
 import * as rimraf from "rimraf";
-import { Worker } from "worker_threads";
 import { exec } from "child_process";
-import { cpus } from "os";
 
 import { convertGraphToTree } from "./graphToTree";
 import { executeTree } from "./treeExecutor";
+import { copyFiles } from "./copyFiles";
 
 import type { Graph } from "./graph";
 export type { Graph } from "./graph";
@@ -20,81 +19,6 @@ const cmdShim: (
   from: string,
   to: string
 ) => Promise<void> = require("cmd-shim");
-
-function spawnWorker() {
-  return new Worker(path.join(__dirname, "copy.js"));
-}
-
-const maxWorkers = process.env.WORKERS_LIMIT
-  ? parseInt(process.env.WORKERS_LIMIT)
-  : 999;
-const numberOfWorkers = Math.min(maxWorkers, Math.ceil(cpus().length / 2));
-
-function createWorkers() {
-  const workers = [];
-
-  for (let i = 0; i < numberOfWorkers; i++) {
-    const worker = spawnWorker();
-    workers.push(worker);
-  }
-  return workers;
-}
-
-async function copyFiles(fileActions: { src: string; dest: string }[]) {
-  const workers = createWorkers();
-  await new Promise<void>((resolve, reject) => {
-    const split = fileActions
-      .reduce<{ src: string; dest: string }[][]>(
-        (acc, curr) => {
-          if (
-            acc[acc.length - 1].length <
-            Math.ceil(fileActions.length / numberOfWorkers)
-          ) {
-            acc[acc.length - 1].push(curr);
-          } else {
-            acc.push([curr]);
-          }
-          return acc;
-        },
-        [[]]
-      )
-      .filter((ac) => ac && ac.length);
-
-    if (!split.length) {
-      resolve();
-    }
-
-    let running = 0;
-    split.forEach((ac, i) => {
-      const worker = workers[i];
-      const onError = (err: any) => {
-        worker.off("error", onError);
-        worker.off("close", onError);
-        worker.off("message", onMessage);
-        reject(err && err.err);
-      };
-      const onMessage = () => {
-        running -= 1;
-        if (running === 0) {
-          resolve();
-        }
-        worker.off("error", onError);
-        worker.off("close", onError);
-        worker.off("message", onMessage);
-      };
-      worker.on("error", onError);
-      worker.on("close", onError);
-      worker.on("message", onMessage);
-      const actions = ac.map((action) => ({
-        src: action.src,
-        dest: action.dest,
-      }));
-
-      running++;
-      worker.postMessage({ actions });
-    });
-  }).finally(() => workers.forEach((w) => w.terminate()));
-}
 
 function rmdir(dir: string): Promise<void> {
   return new Promise<void>((resolve, reject) => {
